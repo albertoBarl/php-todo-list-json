d436fc16ecf1d21d9c2b34469bc31882e15b11be
albertoBarl <alberto.barletta00@gmail.com>
2023-02-05 16:10:09 +0100

    added js file

--- index.php
+++ index.php
@@ -9,9 +9,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To-Do List</title>
+    <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- vuejs -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.2/axios.min.js"></script>
+
 </head>
 <body class="bg-dark">
-    
+    <div id="app">
+        
+    </div>
 </body>
 </html>
